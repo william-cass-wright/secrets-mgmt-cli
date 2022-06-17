065769e24569445bfe00e5e427f19e84dee742cc
william-cass-wright <william.cass.wright@gmail.com>
2022-06-17 01:05:22 -0700

    prep for crud endpoints

--- secrets_mgmt_cli/aws.py
+++ secrets_mgmt_cli/aws.py
@@ -1,32 +1,5 @@
 """
-https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/secretsmanager.html
-
-# aws = AwsSecretMgmt()
-# secret_name = "test/test_secret"
-# secret = aws.get_secret(secret_name)
-
-# secret_name = 'test/test_secret_2'
-# secret_string = json.dumps(secret)
-# aws.create_secret(secret_string,secret_name)
-
-## write aws secrets to local
-
-from .aws import AwsSecretMgmt
-
-aws_secret = AwsSecretMgmt()
-
-def write_secret_to_local_config(project_name):
-    config = ConfigHandler(project_name)
-    secrets_prefix = "projects/dev"
-    secret = aws_secret.get_secret(os.path.join(secrets_prefix, project_name))
-    config.write_config_file_from_dict(config_dict=secret)
-    return config.print_configs()
-
-for project_name in projects:
-    write_secret_to_local_config(project_name)
-
-    
-https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+docstring
 """
 
 import os
@@ -52,7 +25,6 @@ def get_default_region() -> str:
 
 class AwsSecretMgmt:
     def __init__(self):
-        # Create a Secrets Manager client
         session = boto3.session.Session()
         self.client = session.client(service_name="secretsmanager", region_name=get_default_region())
 
